6cc171d966034a5f37f04cb5abcf0b78580152b3
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-03-24 19:23:23 -0700

    Rename values and types to better distinguish what they hold

--- test/Test/Data/Veither.purs
+++ test/Test/Data/Veither.purs
@@ -22,8 +22,8 @@ type X r = (x :: Int | r)
 type Y r = (y :: Int | r)
 
 type E0 = () :: Row Type
-type E1 = X ()
-type E2 = X (Y ())
+type E1_X = X ()
+type E2_XY = X (Y ())
 
 _x = Proxy :: Proxy "x"
 _y = Proxy :: Proxy "y"
@@ -32,28 +32,28 @@ a = 10 :: Int
 x = 20 :: Int
 y = 30 :: Int
 
-v0a =             pure a :: Veither E0 Int
+v0a =                pure a :: Veither E0 Int
 
 -- these are only used in the `vhandle` test
-v0x =             pure x :: Veither E0 Int
-v0y =             pure y :: Veither E0 Int
+v0x =                pure x :: Veither E0 Int
+v0y =                pure y :: Veither E0 Int
 
-v1a =             pure a :: Veither E1 Int
-v1x = Veither $ inj _x x :: Veither E1 Int
+v1_x_a =             pure a :: Veither E1_X Int
+v1_x_x = Veither $ inj _x x :: Veither E1_X Int
 
-v2a =             pure a :: Veither E2 Int
-v2x = Veither $ inj _x x :: Veither E2 Int
-v2y = Veither $ inj _y y :: Veither E2 Int
+v2_xy_a =             pure a :: Veither E2_XY Int
+v2_xy_x = Veither $ inj _x x :: Veither E2_XY Int
+v2_xy_y = Veither $ inj _y y :: Veither E2_XY Int
 
-onX :: forall a. (Int -> a) -> Variant E1 -> a
-onX doX = 
-  case_ 
+onX :: forall a. (Int -> a) -> Variant E1_X -> a
+onX doX =
+  case_
     # on _x doX
 
-onXY :: forall a. (Int -> a) -> (Int -> a) -> Variant E2 -> a
-onXY doX doY = 
-  case_ 
-    # on _x doX 
+onXY :: forall a. (Int -> a) -> (Int -> a) -> Variant E2_XY -> a
+onXY doX doY =
+  case_
+    # on _x doX
     # on _y doY
 
 addOne :: Int -> Int
@@ -73,94 +73,94 @@ spec = do
   describe "Standard functions" do
     it "veither works" do
       veither case_                    identity v0a `shouldEqual` a
-      veither (onX identity) identity v1a `shouldEqual` a
-      veither (onX identity) identity v1x `shouldEqual` x
-      veither (onXY identity identity) identity v2a `shouldEqual` a
-      veither (onXY identity identity) identity v2x `shouldEqual` x
-      veither (onXY identity identity) identity v2y `shouldEqual` y
+      veither (onX identity) identity v1_x_a `shouldEqual` a
+      veither (onX identity) identity v1_x_x `shouldEqual` x
+      veither (onXY identity identity) identity v2_xy_a `shouldEqual` a
+      veither (onXY identity identity) identity v2_xy_x `shouldEqual` x
+      veither (onXY identity identity) identity v2_xy_y `shouldEqual` y
     it "vsafe works" do
       vsafe (pure 1) `shouldEqual` 1
     it "vhandle works" do
-      vhandle _x identity v1a `shouldEqual` v0a
-      vhandle _x identity v1x `shouldEqual` v0x
+      vhandle _x identity v1_x_a `shouldEqual` v0a
+      vhandle _x identity v1_x_x `shouldEqual` v0x
 
-      vhandle _y identity (vhandle _x identity v2a) `shouldEqual` v0a
-      vhandle _y identity (vhandle _x identity v2x) `shouldEqual` v0x
-      vhandle _y identity (vhandle _x identity v2y) `shouldEqual` v0y
+      vhandle _y identity (vhandle _x identity v2_xy_a) `shouldEqual` v0a
+      vhandle _y identity (vhandle _x identity v2_xy_x) `shouldEqual` v0x
+      vhandle _y identity (vhandle _x identity v2_xy_y) `shouldEqual` v0y
 
-      vhandle _x identity (vhandle _y identity v2a) `shouldEqual` v0a
-      vhandle _x identity (vhandle _y identity v2x) `shouldEqual` v0x
-      vhandle _x identity (vhandle _y identity v2y) `shouldEqual` v0y
+      vhandle _x identity (vhandle _y identity v2_xy_a) `shouldEqual` v0a
+      vhandle _x identity (vhandle _y identity v2_xy_x) `shouldEqual` v0x
+      vhandle _x identity (vhandle _y identity v2_xy_y) `shouldEqual` v0y
     it "vfromEither works" do
-      vfromEither _x er10 `shouldEqual` v1a
-      vfromEither _x el20 `shouldEqual` v1x
-      vfromEither _y er10 `shouldEqual` v2a
-      vfromEither _y el30 `shouldEqual` v2y
+      vfromEither _x er10 `shouldEqual` v1_x_a
+      vfromEither _x el20 `shouldEqual` v1_x_x
+      vfromEither _y er10 `shouldEqual` v2_xy_a
+      vfromEither _y el30 `shouldEqual` v2_xy_y
     it "fromRight works" do
       vfromRight 0 v0a `shouldEqual` a
 
-      vfromRight 0 v1a `shouldEqual` a
-      vfromRight 0 v1x `shouldEqual` 0
-      
-      vfromRight 0 v2a `shouldEqual` a
-      vfromRight 0 v2x `shouldEqual` 0
-      vfromRight 0 v2y `shouldEqual` 0
+      vfromRight 0 v1_x_a `shouldEqual` a
+      vfromRight 0 v1_x_x `shouldEqual` 0
+
+      vfromRight 0 v2_xy_a `shouldEqual` a
+      vfromRight 0 v2_xy_x `shouldEqual` 0
+      vfromRight 0 v2_xy_y `shouldEqual` 0
     it "fromRight' works" do
       vfromRight' (\_ -> 0) v0a `shouldEqual` a
 
-      vfromRight' (\_ -> 0) v1a `shouldEqual` a
-      vfromRight' (\_ -> 0) v1x `shouldEqual` 0
-      
-      vfromRight' (\_ -> 0) v2a `shouldEqual` a
-      vfromRight' (\_ -> 0) v2x `shouldEqual` 0
-      vfromRight' (\_ -> 0) v2y `shouldEqual` 0
+      vfromRight' (\_ -> 0) v1_x_a `shouldEqual` a
+      vfromRight' (\_ -> 0) v1_x_x `shouldEqual` 0
+
+      vfromRight' (\_ -> 0) v2_xy_a `shouldEqual` a
+      vfromRight' (\_ -> 0) v2_xy_x `shouldEqual` 0
+      vfromRight' (\_ -> 0) v2_xy_y `shouldEqual` 0
     it "fromLeft works" do
       vfromLeft 0 case_                  v0a `shouldEqual` 0
-      
-      vfromLeft 0 (onX (_ + 1))          v1a `shouldEqual` 0
-      vfromLeft 0 (onX (_ + 1))          v1x `shouldEqual` (x + 1)
-      
-      vfromLeft 0 (onXY (_ + 1) (_ + 2)) v2a `shouldEqual` 0
-      vfromLeft 0 (onXY (_ + 1) (_ + 2)) v2x `shouldEqual` (x + 1)
-      vfromLeft 0 (onXY (_ + 1) (_ + 2)) v2y `shouldEqual` (y + 2)
+
+      vfromLeft 0 (onX (_ + 1))          v1_x_a `shouldEqual` 0
+      vfromLeft 0 (onX (_ + 1))          v1_x_x `shouldEqual` (x + 1)
+
+      vfromLeft 0 (onXY (_ + 1) (_ + 2)) v2_xy_a `shouldEqual` 0
+      vfromLeft 0 (onXY (_ + 1) (_ + 2)) v2_xy_x `shouldEqual` (x + 1)
+      vfromLeft 0 (onXY (_ + 1) (_ + 2)) v2_xy_y `shouldEqual` (y + 2)
     it "fromLeft' works" do
       vfromLeft' (\_ -> 0) case_                  v0a `shouldEqual` 0
-      
-      vfromLeft' (\_ -> 0) (onX (_ + 1))          v1a `shouldEqual` 0
-      vfromLeft' (\_ -> 0) (onX (_ + 1))          v1x `shouldEqual` (x + 1)
-      
-      vfromLeft' (\_ -> 0) (onXY (_ + 1) (_ + 2)) v2a `shouldEqual` 0
-      vfromLeft' (\_ -> 0) (onXY (_ + 1) (_ + 2)) v2x `shouldEqual` (x + 1)
-      vfromLeft' (\_ -> 0) (onXY (_ + 1) (_ + 2)) v2y `shouldEqual` (y + 2)
+
+      vfromLeft' (\_ -> 0) (onX (_ + 1))          v1_x_a `shouldEqual` 0
+      vfromLeft' (\_ -> 0) (onX (_ + 1))          v1_x_x `shouldEqual` (x + 1)
+
+      vfromLeft' (\_ -> 0) (onXY (_ + 1) (_ + 2)) v2_xy_a `shouldEqual` 0
+      vfromLeft' (\_ -> 0) (onXY (_ + 1) (_ + 2)) v2_xy_x `shouldEqual` (x + 1)
+      vfromLeft' (\_ -> 0) (onXY (_ + 1) (_ + 2)) v2_xy_y `shouldEqual` (y + 2)
     it "vnote works" do
-      vnote _x x (Just a) `shouldEqual` v1a
-      vnote _x x Nothing `shouldEqual` v1x
+      vnote _x x (Just a) `shouldEqual` v1_x_a
+      vnote _x x Nothing `shouldEqual` v1_x_x
 
 
-      vnote _x x (Just a) `shouldEqual` v2a
-      vnote _x x Nothing `shouldEqual` v2x
-      
-      vnote _y y (Just a) `shouldEqual` v2a
-      vnote _y y Nothing `shouldEqual` v2y
+      vnote _x x (Just a) `shouldEqual` v2_xy_a
+      vnote _x x Nothing `shouldEqual` v2_xy_x
+
+      vnote _y y (Just a) `shouldEqual` v2_xy_a
+      vnote _y y Nothing `shouldEqual` v2_xy_y
     it "vnote' works" do
-      vnote' _x (\_ -> x) (Just a) `shouldEqual` v1a
-      vnote' _x (\_ -> x) Nothing `shouldEqual` v1x
+      vnote' _x (\_ -> x) (Just a) `shouldEqual` v1_x_a
+      vnote' _x (\_ -> x) Nothing `shouldEqual` v1_x_x
 
 
-      vnote' _x (\_ -> x) (Just a) `shouldEqual` v2a
-      vnote' _x (\_ -> x) Nothing `shouldEqual` v2x
+      vnote' _x (\_ -> x) (Just a) `shouldEqual` v2_xy_a
+      vnote' _x (\_ -> x) Nothing `shouldEqual` v2_xy_x
 
-      vnote' _y (\_ -> y) (Just a) `shouldEqual` v2a
-      vnote' _y (\_ -> y) Nothing `shouldEqual` v2y
+      vnote' _y (\_ -> y) (Just a) `shouldEqual` v2_xy_a
+      vnote' _y (\_ -> y) Nothing `shouldEqual` v2_xy_y
     it "vhush works" do
       vhush v0a `shouldEqual` Just a
 
-      vhush v1a `shouldEqual` Just a
-      vhush v1x `shouldEqual` Nothing
+      vhush v1_x_a `shouldEqual` Just a
+      vhush v1_x_x `shouldEqual` Nothing
 
-      vhush v2a `shouldEqual` Just a
-      vhush v2x `shouldEqual` Nothing
-      vhush v2y `shouldEqual` Nothing      
+      vhush v2_xy_a `shouldEqual` Just a
+      vhush v2_xy_x `shouldEqual` Nothing
+      vhush v2_xy_y `shouldEqual` Nothing
 
 checkVeitherTypeClassLaws :: Effect Unit
 checkVeitherTypeClassLaws = do
